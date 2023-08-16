d8f8a734f586cbe0eed0a93b2471889f84ef4b46
nnnLik <rslmakhmudov@gmail.com>
2023-08-16 22:14:16 +0300

    [EDIT] update status of token

--- server/src/core/urls.py
+++ server/src/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import GetGeneratedToken
+from .views import GetGeneratedTokenView, UpdateTokenStatusView, SendMessageView
 
 urlpatterns = [
-    path("generate/", GetGeneratedToken.as_view(), name="generate"),
+    path("generate/", GetGeneratedTokenView.as_view(), name="generate-token"),
+    path("update-status/", UpdateTokenStatusView.as_view(), name="update-token-status"),
+    path("send/", SendMessageView.as_view(), name="send-message"),
 ]
