3b9e90d730da5e6aff7a3ffd1f12eaab7842360b
nnnLik <rslmakhmudov@gmail.com>
2023-08-16 22:15:41 +0300

    [EDIT] entrypoint

--- tg-bot/main.py
+++ tg-bot/main.py
@@ -1,26 +1,15 @@
+from aiogram.utils import executor
 import logging
-import aiogram.utils.markdown as md
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ParseMode
+import sys
 
-API_TOKEN = "6360409566:AAE0l-dCI3OEtLz0nuJROz-iV8DvYZuC1Qg"
+from bot import dp, bot
+from src.handlers import setup
 
-logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
+async def on_startup(dp):
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    setup(dp, bot)
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    text = md.text(md.text('Привет, {0.first_name}!\nЯ телеграм-бот, который повторяет ваше сообщение.'.format(message.from_user), md.bold('Жду вашего сообщения...')))
-    await message.reply(text, parse_mode=ParseMode.MARKDOWN)
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer("Вы сказали: " + message.text)
-
-if __name__ == '__main__':
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
