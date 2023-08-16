a5c4038f13c37a30a4911c0aac58cc498d6298a8
nnnLik <rslmakhmudov@gmail.com>
2023-08-16 22:14:26 +0300

    [ADD] migrations

--- server/src/core/migrations/0002_alter_telegramtoken_token.py
+++ server/src/core/migrations/0002_alter_telegramtoken_token.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('core', '0001_initial'),
+        ("core", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='telegramtoken',
-            name='token',
+            model_name="telegramtoken",
+            name="token",
             field=models.CharField(max_length=256, unique=True),
         ),
     ]
