3410dffe6710ead5edddd623387658f75c0ddefe
nnnLik <rslmakhmudov@gmail.com>
2023-08-16 22:15:30 +0300

    [ADD] history msg

--- server/src/core/services.py
+++ server/src/core/services.py
@@ -3,11 +3,13 @@ from typing import Dict
 
 from django.contrib.auth.models import User
 
+from rest_framework import status
+
 from .serializers import TelegramTokenSerializer
 from .models import TelegramToken
 
 
-class TokenGenerator:
+class TokenServices:
     def generate(self, user: User) -> Dict[str, str]:
         self._delete_token_if_exists(user)
         token: str = self._generate_new_token()
@@ -17,6 +19,23 @@ class TokenGenerator:
 
         return result
 
+    def update_token_status(
+        self,
+        chat_id: int,
+        username: str,
+        token: str,
+    ) -> None:
+        try:
+            telegram_token = TelegramToken.objects.get(token=token)
+        except TelegramToken.DoesNotExist:
+            return {
+                "error": "Token not found for the given User."
+            }, status.HTTP_400_BAD_REQUEST
+
+        telegram_token.activate_token(chat_id=chat_id, telegram_username=username)
+
+        return {"message": "Token updated successfully"}, status.HTTP_200_OK
+
     def _delete_token_if_exists(self, user: User) -> None:
         TelegramToken.objects.filter(user=user).delete()
 
@@ -25,3 +44,8 @@ class TokenGenerator:
 
     def _create_telegram_token(self, user: User, token: str):
         return TelegramToken.objects.create(user=user, token=token)
+
+
+class MessageServices:
+    def create_msg(self, user: User, msg: str):
+        ...
